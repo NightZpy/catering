b8dc7b7cdb4378cc126a64919e97a10cd9b6f9d4
Lenyn Alcántara <nightzpy@gmail.com>
2016-08-18 11:23:02 -0300

    disable foreign keys check in item_provider seed

--- database/seeds/RecipeTypeTableSeeder.php
+++ database/seeds/RecipeTypeTableSeeder.php
@@ -12,12 +12,15 @@ class RecipeTypeTableSeeder extends Seeder
      */
     public function run()
     {
-    	RecipeType::truncate();
+        \Eloquent::unguard();
+        \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        RecipeType::truncate();
         for ($i=1; $i <= 20; $i++) { 
             $recipeTypes = [
                 'name' => "RecipeType-$i"
             ];
             RecipeType::create($recipeTypes);
         }
+        \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
