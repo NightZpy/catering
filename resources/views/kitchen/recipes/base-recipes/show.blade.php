265867ea730ccb360ae7d07aef0bd9cc4da44378
Lenyn Alcántara <nightzpy@gmail.com>
2016-10-09 12:33:35 -0300

    Solve bug with bad Model assoc to base-recipe relation into recipe index form

--- resources/views/kitchen/recipes/base-recipes/show.blade.php
+++ resources/views/kitchen/recipes/base-recipes/show.blade.php
@@ -1,10 +1,10 @@
 <?php 
 	extract([
-            'modalTitle'    => 'Details for {{ row.utensil.name }}',
-            'modalSync'     => 'utensilSHOW',
-            'modalClose'    => "'utensilSHOW'",
-            'model'         => 'utensil',
-            'related'       => "'utensil'",
+            'modalTitle'    => 'Details for {{ row.base.name }}',
+            'modalSync'     => 'baseSHOW',
+            'modalClose'    => "'baseSHOW'",
+            'model'         => 'Base',
+            'related'       => "'base'",
             //'content'       => view('kitchen.utensils.utensils.fields')
 	]);
 ?>
