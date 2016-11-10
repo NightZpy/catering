6052b75bd1d2f667ac8649f878926c7b919b6f6b
ajpadilla <ajpadilla88@gmail.com>
2016-11-09 23:04:02 -0400

    Agregando archivo para traducciones de modals y layouts

---
+++ resources/lang/en/modals.php
@@ -0,0 +1,14 @@
+<?php
+	return[
+		'template' => [
+			'form-add-title' => 'Datos unidad',
+			'form-show-title' => 'Unidad',
+			'form-delete-title' => 'Eliminar Unidad',
+			'buttons' => [
+				'close' => 'Cerrar',
+				'save' => 'Guardar',
+				'cancel' => 'Cancelar',
+				'accept' => 'Aceptar'
+			]
+		]
+	];
