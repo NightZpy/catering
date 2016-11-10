39e6b43bf423617593d815b5389ee244dcd75c37
ajpadilla <ajpadilla88@gmail.com>
2016-11-10 12:50:41 -0400

    Agregando traducciones para modelo familia del proyecto

---
+++ resources/lang/es/families.php
@@ -0,0 +1,31 @@
+<?php  
+	return [
+		'index' => [
+			'main-title' => 'Familias',
+			'buttons' => [
+				'add-new' => 'Añadir Nueva',
+				'search' => 'Buscar',
+				'reset' => 'Reiniciar',
+				'simple' => 'Simple',
+				'detail' => 'Detallado',
+				'dropdown' => 'Desplegable', 
+			],
+			'labels' => [
+				'pagination-style' => 'Estilo de paginación:',
+				'items-per-page' => 'Artículos por página:',
+				'name' => 'nombre'
+			],
+			'messages' => [
+				'success' => 'Familia recuperada correctamente',
+				'failed' => 'Familia no encontrada',
+				'saved' => 'Familia agregada correctamente',
+				'updated' => 'Familia actualizada correctamente', 
+				'deleted' => 'Familia eliminada correctamente'
+			],
+		], 
+		'template' => [
+			'form-add-title' => 'Datos de la familia',
+			'form-show-title' => 'Familia',
+			'form-delete-title' => 'Eliminar familia!',
+		]
+	];
