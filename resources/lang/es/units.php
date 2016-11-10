cd399702e3e297ffffa762beb7074e7354de2624
ajpadilla <ajpadilla88@gmail.com>
2016-11-09 23:03:33 -0400

    Eliminando sección template

--- resources/lang/es/units.php
+++ resources/lang/es/units.php
@@ -22,16 +22,5 @@
 				'updated' => 'Unidad actualizada correctamente', 
 				'deleted' => 'Unidad eliminada correctamente'
 			],
-		], 
-		'template' => [
-			'form-add-title' => 'Datos unidad',
-			'form-show-title' => 'Unidad',
-			'form-delete-title' => 'Eliminar Unidad',
-			'buttons' => [
-				'close' => 'Cerrar',
-				'save' => 'Guardar',
-				'cancel' => 'Cancelar',
-				'accept' => 'Aceptar'
-			]
 		]
 	];
