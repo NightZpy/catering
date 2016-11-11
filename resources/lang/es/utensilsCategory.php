d16c681c5c0a689d4d8f297906e3ef5f7f244403
ajpadilla <ajpadilla88@gmail.com>
2016-11-11 15:18:04 -0400

    Agregando traducciones para modelo utensilCategory

---
+++ resources/lang/es/utensilsCategory.php
@@ -0,0 +1,31 @@
+<?php  
+	return [
+		'index' => [
+			'main-title' => 'Categorías de utensilios',
+			'buttons' => [
+				'add-new' => 'Añadir nuevo',
+				'search' => 'Buscar',
+				'reset' => 'Reiniciar',
+				'simple' => 'Simple',
+				'detail' => 'Detallado',
+				'dropdown' => 'Desplegable', 
+			],
+			'labels' => [
+				'pagination-style' => 'Estilo de paginación:',
+				'items-per-page' => 'Artículos por página:',
+				'name' => 'nombre'
+			],
+			'messages' => [
+				'success' => 'Categoría de utensilio recuperado correctamente',
+				'failed' => 'Categoría de utensilio no encontrado',
+				'saved' => 'Categoría de utensilio agregado correctamente',
+				'updated' => 'Categoría de utensilio actualizado correctamente', 
+				'deleted' => 'Categoría de utensilio eliminado correctamente'
+			],
+		], 
+		'template' => [
+			'form-add-title' => 'Datos para categoría de utensilio',
+			'form-show-title' => 'Categoría de utensilio',
+			'form-delete-title' => '¡Eliminar categoría de utensilio!',
+		]
+	];
