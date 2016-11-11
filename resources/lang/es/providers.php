cbd4d647eba4ce89f3a9e15f6ea464a224744ff0
ajpadilla <ajpadilla88@gmail.com>
2016-11-11 13:03:35 -0400

    Agregando traducciones para proveedores

---
+++ resources/lang/es/providers.php
@@ -0,0 +1,31 @@
+<?php  
+	return [
+		'index' => [
+			'main-title' => 'Proveedores',
+			'buttons' => [
+				'add-new' => 'Añadir nuevo',
+				'search' => 'Buscar',
+				'reset' => 'Reiniciar',
+				'simple' => 'Simple',
+				'detail' => 'Detallado',
+				'dropdown' => 'Desplegable', 
+			],
+			'labels' => [
+				'pagination-style' => 'Estilo de paginación:',
+				'items-per-page' => 'Artículos por página:',
+				'name' => 'nombre'
+			],
+			'messages' => [
+				'success' => 'Proveedor recuperado correctamente',
+				'failed' => 'Proveedor no encontrado',
+				'saved' => 'Proveedor agregado correctamente',
+				'updated' => 'Proveedor actualizado correctamente', 
+				'deleted' => 'Proveedor eliminado correctamente'
+			],
+		], 
+		'template' => [
+			'form-add-title' => 'Proveedor',
+			'form-show-title' => 'Proveedor',
+			'form-delete-title' => '¡Eliminar proveedor!',
+		]
+	];
