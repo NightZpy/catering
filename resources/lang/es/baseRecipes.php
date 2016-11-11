e99b0c2175df48d2455bbb7b66ada92b6703b59b
ajpadilla <ajpadilla88@gmail.com>
2016-11-11 16:40:38 -0400

    Actualizando traducciones para modelo baseRecipes

---
+++ resources/lang/es/baseRecipes.php
@@ -0,0 +1,39 @@
+<?php  
+	return [
+		'index' => [
+			'main-title' => 'Recetas bases',
+			'buttons' => [
+				'add-new' => 'Añadir Nueva',
+				'search' => 'Buscar',
+				'reset' => 'Reiniciar',
+				'simple' => 'Simple',
+				'detail' => 'Detallado',
+				'dropdown' => 'Desplegable', 
+			],
+			'labels' => [
+				'pagination-style' => 'Estilo de paginación:',
+				'items-per-page' => 'Artículos por página:',
+				'name' => 'nombre'
+			],
+			'messages' => [
+				'success' => 'Receta base recuperada correctamente',
+				'failed' => 'Receta base no encontrada',
+				'saved' => 'Receta base agregada correctamente',
+				'updated' => 'Receta base actualizada correctamente', 
+				'deleted' => 'Receta base eliminada correctamente'
+			],
+		], 
+		'show' => [
+			'labels' => [
+				'list' => 'Lista',
+				'Utensils' => 'Utensilios',
+				'Description' => 'Descripción',
+				'Recipe' => 'Receta'			
+			],
+		],
+		'template' => [
+			'form-add-title' => 'Datos de la receta base',
+			'form-show-title' => 'Receta base',
+			'form-delete-title' => '¡Eliminar receta base!',
+		]
+	];
