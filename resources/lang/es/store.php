0ffb5beb1c547d7807f5d3fe75794015acd99750
ajpadilla <ajpadilla88@gmail.com>
2016-11-11 12:14:51 -0400

    Agregando traducciones para modelo items del proyecto

---
+++ resources/lang/es/store.php
@@ -0,0 +1,31 @@
+<?php  
+	return [
+		'index' => [
+			'main-title' => 'Almacen',
+			'buttons' => [
+				'add-new' => 'Añadir nuevo',
+				'search' => 'Buscar',
+				'reset' => 'Reiniciar',
+				'simple' => 'Simple',
+				'detail' => 'Detallado',
+				'dropdown' => 'Desplegable', 
+			],
+			'labels' => [
+				'pagination-style' => 'Estilo de paginación:',
+				'items-per-page' => 'Artículos por página:',
+				'name' => 'nombre'
+			],
+			'messages' => [
+				'success' => 'Artículo recuperado correctamente',
+				'failed' => 'Artículo no encontrado',
+				'saved' => 'Artículo agregado correctamente',
+				'updated' => 'Artículo actualizado correctamente', 
+				'deleted' => 'Artículo eliminado correctamente'
+			],
+		], 
+		'template' => [
+			'form-add-title' => 'Artículo',
+			'form-show-title' => 'Artículo',
+			'form-delete-title' => '¡Eliminar artículo!',
+		]
+	];
