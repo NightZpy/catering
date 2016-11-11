cad208a85f909558f78ed32b5d98726414829b08
ajpadilla <ajpadilla88@gmail.com>
2016-11-11 14:52:03 -0400

    Agregando traducciones para el modelo utensils del proyecto

---
+++ resources/lang/es/utensils.php
@@ -0,0 +1,31 @@
+<?php  
+	return [
+		'index' => [
+			'main-title' => 'Utensilios',
+			'buttons' => [
+				'add-new' => 'Añadir nuevo',
+				'search' => 'Buscar',
+				'reset' => 'Reiniciar',
+				'simple' => 'Simple',
+				'detail' => 'Detallado',
+				'dropdown' => 'Desplegable', 
+			],
+			'labels' => [
+				'pagination-style' => 'Estilo de paginación:',
+				'items-per-page' => 'Artículos por página:',
+				'name' => 'nombre'
+			],
+			'messages' => [
+				'success' => 'Utensilios recuperada correctamente',
+				'failed' => 'Utensilios no encontrada',
+				'saved' => 'Utensilios agregada correctamente',
+				'updated' => 'Utensilios actualizada correctamente', 
+				'deleted' => 'Utensilios eliminada correctamente'
+			],
+		], 
+		'template' => [
+			'form-add-title' => 'Datos utensilios',
+			'form-show-title' => 'Utensilios',
+			'form-delete-title' => 'Eliminar utensilios',
+		]
+	];
