66a242f8f3e912606b16661ac325734294c55b53
ajpadilla <ajpadilla88@gmail.com>
2016-11-14 20:21:37 -0400

    Actualizando traducciones para proveedores asociados

--- resources/lang/es/store.php
+++ resources/lang/es/store.php
@@ -20,7 +20,17 @@
 				'failed' => 'Artículo no encontrado',
 				'saved' => 'Artículo agregado correctamente',
 				'updated' => 'Artículo actualizado correctamente', 
-				'deleted' => 'Artículo eliminado correctamente'
+				'deleted' => 'Artículo eliminado correctamente',
+				'item' => 'Artículo no encontrado',
+				'provider' => 'No se encontro proveedor',
+				'provider-associated' => 'Proveedor asociado al artíclulo correctamente',
+				'provider-associated-data' => 'Proveedor recuperado correctamente',
+				'not-provider-item' => 'El artículo no tiene proveedores',
+				'provider-retrieve' => 'Proveedor del artículo encontrado',
+				'provider-already-associate' => 'El proveedor ya está asociado al artículo',
+				'provider-not-associate' => 'El proveedor no esta asociado todavía al artículo',
+				'provider-detached' => 'Proveedor eliminado correctamente del artículo asociado',
+				'provider-not-detached' => 'No se pudo eliminar proveedor asociado al artículo'
 			],
 		], 
 		'template' => [
