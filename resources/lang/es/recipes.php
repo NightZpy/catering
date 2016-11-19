ea481ff27f401d11690dbed805362d9bf6d08ee2
ajpadilla <ajpadilla88@gmail.com>
2016-11-18 21:02:17 -0400

    Agregando traducciones

--- resources/lang/es/recipes.php
+++ resources/lang/es/recipes.php
@@ -20,7 +20,20 @@
 				'failed' => 'Receta no encontrada',
 				'saved' => 'Receta agregada correctamente',
 				'updated' => 'Receta actualizada correctamente', 
-				'deleted' => 'Receta eliminada correctamente'
+				'deleted' => 'Receta eliminada correctamente',
+				'utensil-associated' => 'Utensilio asociado correctamente a la receta',
+				'utensil-not-found' => 'No se encontro el Utensilio',
+				'utensil-not-found' => 'No se encontro el Utensilio',
+				'utensil-detached' => 'El utensilio se elimino de receta',
+				'utensil-not-detached' => 'El utensilio no se pudo eliminar de BaseRecipe',
+				'baseRecipe-failed' => 'No se encontro la receta base',
+				'baseRecipe-success' => 'Receta base asociada correctamente a la receta',
+				'baseRecipe-not-associted' => 'No existe la receta base para la receta',
+				'baseRecipes-not-found' => 'No se encontro la receta base',
+				'baseRecipe-retrieve' => 'Receta base encontrada',
+				'baseRecipe-detached' => 'Receta base eliminada correctamente de la receta',
+				'baseRecipe-not-detached' => 'No se pudo eliminada la receta base de la receta'
+
 			],
 		], 
 		'show' => [
