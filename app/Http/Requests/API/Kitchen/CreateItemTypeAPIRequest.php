7c89cae73fd8f42cb668010eacd1a3a9f6176462
ajpadilla <ajpadilla88@gmail.com>
2016-11-25 14:40:52 -0400

    Agregando request para validar campos del modelo ItemType

---
+++ app/Http/Requests/API/Kitchen/CreateItemTypeAPIRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\API\Kitchen;
+
+use App\Http\Requests\Request;
+use App\Models\Kitchen\ItemType;
+use App\Http\Requests\API\MyAPIRequest;
+
+class CreateItemTypeAPIRequest extends MyAPIRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return ItemType::$rules;
+    }
+}
