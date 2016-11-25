cdc6c9ffc1a06f91792f4dad8f43e4636280f415
ajpadilla <ajpadilla88@gmail.com>
2016-11-25 11:52:13 -0400

    Agregando modelo ItemType

---
+++ app/Models/Kitchen/ItemType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models\Kitchen;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\SearchTrait;
+use App\Models\SortTrait;
+
+class ItemType extends Model
+{
+
+	use SoftDeletes, SearchTrait, SortTrait;
+
+    protected $searchableColumns = [
+        'name'
+    ];    
+
+    protected $dates = ['deleted_at'];
+
+    protected $appends = [];   
+
+    public $fillable = [
+        'name',
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'string',
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required|min:1|max:128|unique:families',
+    ];
+}
