9f0454655aad4848e6bb16d38f777dfef4d98b9d
ajpadilla <ajpadilla88@gmail.com>
2016-11-25 13:50:05 -0400

    Agregando campo item_type_id

---
+++ database/migrations/2016_11_25_172651_add_item_tipe_id_to_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddItemTipeIdToItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->integer('item_type_id')->unsigned()->index()->after('type');
+            $table->foreign('item_type_id')->references('id')->on('item_types')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropForeign('items_item_type_id_foreign');
+            $table->dropColumn('item_type_id');
+        });
+    }
+}
